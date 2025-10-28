a11394105cb2483244c4fa5e42ab269bb3586b20
its-sher <39632735+itsmevky@users.noreply.github.com>
2025-10-28 12:26:40 +0530

    update

--- client/src/Accountant/accountantdashboard.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState, useRef } from "react";
-
-const domainpath = process.env.REACT_APP_API_DOMAIN_ENDPOINT;
-const accountantDashboard = () => {
-  return <div className="p-8">hjvjhvbn nbnchg</div>;
-};
-
-export default accountantDashboard;
