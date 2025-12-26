da9b1ca997bf384c86ececf263b1f2cd632a124c
sahil-ajiva <sahilajiva@gmail.com>
2025-12-26 19:06:10 +0530

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,7 @@ const corsOptions = {
     "http://localhost:3002",
     "http://localhost:5000",
     "http://localhost:4001",
+    "https://pidiliteapp.ajivainfotech.com"
   ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   allowedHeaders: "Content-Type, Authorization",
