3da6848a45cb26322a1401566851a04a32c6dfa3
sahil-ajiva <sahilajiva@gmail.com>
2025-12-31 10:42:51 +0530

    commit

--- client/src/Modules/AppQr/AppQrPage.js
+++ client/src/Modules/AppQr/AppQrPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import QRCode from "qrcode";
 import logo from "../../Images/pidilite-logo-13.png";
 
-const TARGET_URL = "https://pidiliteapp.ajivainfotech.com/";
+const TARGET_URL = "https://pidiliteapp.ajivainfotech.com/qrdownload";
 
 const AppQrPage = () => {
   const [qrSrc, setQrSrc] = useState("");
