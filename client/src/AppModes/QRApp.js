68dae3490d91c3fbbd05166e8084f8961a7eb764
sahil-ajiva <sahilajiva@gmail.com>
2026-03-09 16:08:14 +0530

    ui of qrapp updated

--- client/src/AppModes/QRApp.js
+++ client/src/AppModes/QRApp.js
@@ -3,10 +3,8 @@ import WebView from './WebView';
 
 const QRApp = () => {
     return (
-        <div className="fixed inset-0 flex w-full h-full overflow-hidden bg-black font-sans">
-            <div className="flex-grow h-full w-full relative z-0">
-                <WebView url="/qr?mode=app" allowPermission="camera" />
-            </div>
+        <div className="fixed inset-0 w-full h-full overflow-hidden bg-white font-sans">
+            <WebView url="/qr?mode=app" allowPermission="camera" />
         </div>
     );
 };
--- client/src/AppModes/WebView.js
+++ client/src/AppModes/WebView.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 const WebView = ({ url, allowPermission = "camera; microphone; geolocation" }) => {
     return (
-        <div className="w-full h-full">
-            <iframe
-                src={url}
-                title="Web View"
-                className="w-full h-full border-none"
-                style={{ width: '100%', height: '100%', border: 'none' }}
-                allow={allowPermission}
-            />
-        </div>
+        <iframe
+            src={url}
+            title="Web View"
+            className="w-full h-full border-none"
+            style={{
+                width: '100%',
+                height: '100%',
+                border: 'none',
+                display: 'block',
+                margin: 0,
+                padding: 0
+            }}
+            allow={allowPermission}
+        />
     );
 };
 
