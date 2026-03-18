36aeeed4db461ccc8db0de0c8a70b51d29fd0cc6
sahil-ajiva <sahilajiva@gmail.com>
2026-03-18 13:58:21 +0530

    download link added play store

--- client/src/Components/Website/DownloadApp.js
+++ client/src/Components/Website/DownloadApp.js
@@ -20,8 +20,9 @@ const DownloadApp = () => {
 
                 {/* ANDROID BUTTON */}
                 <a
-                    href="https://pidilite.ajivainfotech.com/apk/pidilite.apk"
-                    download
+                    href="https://play.google.com/store/apps/details?id=com.app.pidilite_app"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="pid_Qr_btn pid_Qr_android"
                 >
                     <svg
@@ -34,10 +35,11 @@ const DownloadApp = () => {
                     Download Android APK
                 </a>
 
-                {/* IOS BUTTON */}
-                <a
-                    href="https://yourdomain.com/ios/app"
+                {/* IOS BUTTON - DISABLED BUT MATCHING ORIGINAL COLOR */}
+                <button
                     className="pid_Qr_btn pid_Qr_ios"
+                    style={{ opacity: 0.7, cursor: 'not-allowed' }}
+                    disabled
                 >
                     <svg
                         width={28}
@@ -47,12 +49,9 @@ const DownloadApp = () => {
                         viewBox="0 0 640 640">
                         <path d="M447.1 332.7C446.9 296 463.5 268.3 497.1 247.9C478.3 221 449.9 206.2 412.4 203.3C376.9 200.5 338.1 224 323.9 224C308.9 224 274.5 204.3 247.5 204.3C191.7 205.2 132.4 248.8 132.4 337.5C132.4 363.7 137.2 390.8 146.8 418.7C159.6 455.4 205.8 545.4 254 543.9C279.2 543.3 297 526 329.8 526C361.6 526 378.1 543.9 406.2 543.9C454.8 543.2 496.6 461.4 508.8 424.6C443.6 393.9 447.1 334.6 447.1 332.7zM390.5 168.5C417.8 136.1 415.3 106.6 414.5 96C390.4 97.4 362.5 112.4 346.6 130.9C329.1 150.7 318.8 175.2 321 202.8C347.1 204.8 370.9 191.4 390.5 168.5z" /></svg>
                     Download iOS
-                </a>
+                </button>
 
-                <p className="pid_Qr_note">
-                    * Android APK may show a Chrome security warning. This is normal.
-                </p>
-
+                {/* Note removed */}
             </div>
         </div>
     );
